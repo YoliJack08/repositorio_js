d06e75e4cf03de5f656656ccca87da8b91ad0ef5
YoliJack08 <yolotlgar@gmail.com>
2021-10-28 19:49:28 -0500

    Primer commit js

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+let n2 = 8;
+let nombreIngresado = prompt("Ingrese su nombre:");
+let saludo = "¡Bienvenido" + " " + nombreIngresado +"!";
+alert(saludo);
+console.log("Hola" + " " + nombreIngresado)
+let n1 = parseInt(prompt("Escribe un número"));
+let suma = n1 + n2;
+alert(suma);
+console.log("La suma de tu número y el mio da:" + " " + suma);
