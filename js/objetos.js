79d34f2c42ab284ec20890e122d7e5cdf97e8961
YoliJack08 <yolotlgar@gmail.com>
2021-11-09 23:48:08 -0600

    Entrega de objetos

---
+++ js/objetos.js
@@ -0,0 +1,33 @@
+class Usuario {
+    constructor(nombre, apellido, correo, celular, comentario) {
+      this.nombre = nombre.toUpperCase();
+      this.apellido = apellido.toUpperCase();
+      this.correo = correo;
+      this.celular = celular;
+      this.comentario = comentario;
+    }
+
+    VerCorreo(){
+        alert(this.nombre + ", gracias por enviarnos tus valiosos comentarios. Ayudanos a validar si " + this.correo + " está escrito correctamente" )
+    }
+
+  }
+
+  class Producto {
+    constructor(nom_Producto, subPrecio) {
+      this.nom_Producto = nom_Producto;
+      this.subPrecio = parseFloat(subPrecio);
+    }
+
+    precio(){
+       this.subPrecio= this.subPrecio * 1.16;
+       alert ("Gracias! stás por comprar " + this.nom_Producto )
+       alert("Por este producto pagaras $" + this.subPrecio )
+    }
+  }
+
+  let Usuario1 = new Usuario ("carlos", "zepeda", "carlos@gmail.com", 55, "bye");
+  Usuario1.VerCorreo();
+  
+  let Producto1 = new Producto ("Ajolotiux", 200);
+  Producto1.precio();
